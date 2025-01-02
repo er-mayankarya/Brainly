3f83f54878094666906cf64337f9acf55005ec0c
Mayank Arya <ermayankarya7@gmail.com>
2025-01-02 21:34:22 +0530

    DB Schema Created

---
+++ backend/src/db.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema, Types } from "mongoose";
+ 
+// User Schema & Model
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+export const UserModel = mongoose.model("Users", userSchema);
+
+
+// Content Schema & Model
+const contentTypes = ["image", "video", "article", "audio"]; // Extend as needed in future
+
+const contentSchema = new Schema({
+  link: { type: String, required: true },
+  type: { type: String, enum: contentTypes, required: true },
+  title: { type: String, required: true },
+  tags: [{ type: Types.ObjectId, ref: "Tag" }],
+  userId: { type: Types.ObjectId, ref: "User", required: true },
+});
+
+export const ContenTModel = mongoose.model("Contents", contentSchema);
+
+
+// Tag Schema & Model
+const tagSchema = new mongoose.Schema({
+  title: { type: String, required: true, unique: true },
+});
+
+export const TagModel = mongoose.model("Tags", tagSchema);
+
+
+//Link Schema & Model
+const linkSchema = new mongoose.Schema({
+  hash: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+export const LinkModel = mongoose.model("Links", linkSchema);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,54 @@
 import express from 'express';
-const app = express();
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname+'/.env' });
 
+const app = express();
+app.use(express.json());
+
+
+//Sign Up Endpoint
 app.post('/api/v1/signup' , (req , res) => {
 
 });
 
+//Signin Endpoint
 app.post('/api/v1/sigin' , (req , res) => {
 
 });
 
+//Create Content Endpoint
 app.post('/api/v1/content' , (req , res) => {
 
 });
 
+//Get Existing Content Endpoint
 app.get('/api/v1/content' , (req , res) => {
 
 });
 
+//Delete Existing content Endpoint
 app.delete('/api/v1/content' , (req , res) => {
 
 });
 
+// Share Endpint
 app.post('/api/v1/brain/share' , (req , res) => {
 
 });
 
+// Get information with share link Endpoint
 app.get('/api/v1/brain/:shareLink' , (req , res) => {
 
 });
+
+//Server Listen Port - 3000
+async function main(){
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("Connected to DB");
+    app.listen(3000 , () => {
+        console.log("Server listening at port 3000");
+    });
+}
+main();
