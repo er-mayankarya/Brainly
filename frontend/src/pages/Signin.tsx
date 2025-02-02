949fcc9fbe8775b5103fc7a466c2cf280d624b87
Mayank Arya <ermayankarya7@gmail.com>
2025-02-02 09:53:10 +0530

    Updated

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,13 +1,37 @@
+import axios from "axios";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { BACKEND_URL } from "../config";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function Signin(){
+
+    const usernameRef = useRef<HTMLInputElement>();
+    const passwordRef = useRef<HTMLInputElement>();
+    const navigate = useNavigate();
+
+    async function signin(){
+        const username = usernameRef.current?.value;
+        const password = passwordRef.current?.value;
+        const userData = {
+            username , password
+        }
+        
+        const response = await axios.post(BACKEND_URL + "/api/v1/signin" ,  userData)
+        const jwt = response.data.token;
+        localStorage.setItem("token" , jwt);
+        navigate("/dashboard");
+
+    }
+
+
     return <div  className="h-screen w-screen bg-gray-300 flex justify-center items-center">
         <div className="bg-white rounded-xl border min-w-48 p-8">
-            <Input placeholder="Username" />
-            <Input placeholder="Password" />
+            <Input refs={usernameRef} placeholder="Username" />
+            <Input refs={passwordRef} placeholder="Password" />
             <div className="flex justify-center pt-4">
-            <Button variant="primary" text="Signin" fullWidth={true} />
+            <Button onClick={signin} variant="primary" text="Signin" fullWidth={true} />
             </div>
         </div>
 
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -4,11 +4,13 @@ import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
 
 export function Signup(){
 
     const usernameRef = useRef<HTMLInputElement>();
-    const passwordRef = useRef<HTMLInputElement>(); 
+    const passwordRef = useRef<HTMLInputElement>();
+    const navigate = useNavigate();
 
     async function signup(){
         const username = usernameRef.current?.value;
@@ -18,7 +20,8 @@ export function Signup(){
         }
         
         await axios.post(BACKEND_URL + "/api/v1/signup" ,  userData)
-        alert("You are signed up !!")
+        alert("You are signed up !!");
+        navigate("/signin");
 
     }
 
